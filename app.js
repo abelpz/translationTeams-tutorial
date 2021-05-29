74de0a7e9f35ca1de482c3a9a11bf2921e230d3d
Abel <abelper54@gmail.com>
2021-05-29 19:20:10 -0400

    Actualización Bucles y Arrays

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ let nameInput = document.getElementById("nombre");
 let addButton = document.getElementById("add");
 let namesList = document.getElementById("lista-nombres");
 
+let mujeres = ["Angela", "Karina", "Paola"]
+let hombres = ["Elias", "Abel", "Miguel"]
+
+let listaDatos = [mujeres, hombres, 5, "Lucas"]
+
+
+const mostrarElementos = (el) => {
+    
+    if(Array.isArray(el)){
+
+        el.forEach( (x) => { alert(x) } )
+        return
+
+    }else if(el == "Lucas"){
+
+        alert(el + "Lucas")
+
+    }else{
+
+        alert(el)
+
+    }
+
+}
+
+listaDatos.forEach( mostrarElementos )
+
+
 addButton.addEventListener("click", (event) => { //Escucha el evento click y ejecuta una o varias acciones
     event.preventDefault(); //Previene el comportamiento usual de este evento
 
