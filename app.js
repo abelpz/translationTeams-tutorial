f3dd22ef0b1dce3973d57766696bbb5d1c079deb
Abel <abelper54@gmail.com>
2021-06-05 19:41:23 -0400

    Added teams functionality

--- app.js
+++ app.js
@@ -1,46 +1,105 @@
 let nameInput = document.getElementById("nombre");
 let addButton = document.getElementById("add");
-let namesList = document.getElementById("lista-nombres");
+let selectInput = document.getElementById("equipos");
+let teamInput = document.getElementById("nombre-equipo")
+let addTeam = document.getElementById("add-team");
+let teamsList = document.getElementById("lista-equipos");
 
-let mujeres = ["Angela", "Karina", "Paola"]
-let hombres = ["Elias", "Abel", "Miguel"]
+let equipos = []
 
-let listaDatos = [mujeres, hombres, 5, "Lucas"]
+    equipos["equipo1"] = ["Abel", "Elias", "Miguel", "Larry"]
+    equipos["equipo2"] = ["Angela", "Karina", "Paola"]
 
+function addTeamOption(teamName){
 
-const mostrarElementos = (el) => {
-    
-    if(Array.isArray(el)){
+    let option = document.createElement("option");
+        option.setAttribute("value", teamName);
+        option.innerText = teamName;
 
-        el.forEach( (x) => { alert(x) } )
-        return
-
-    }else if(el == "Lucas"){
-
-        alert(el + "Lucas")
-
-    }else{
-
-        alert(el)
-
-    }
+    selectInput.appendChild(option)
 
 }
 
-listaDatos.forEach( mostrarElementos )
+for (var key in equipos){
+
+    addTeamOption(key)
+    showTeams(key, equipos[key])
+    
+}
+
+addTeam.addEventListener("click", (event) => { //Escucha el evento click y ejecuta una o varias acciones
+    event.preventDefault(); //Previene el comportamiento usual de este evento
+
+    if(teamInput.value != ""){
+
+        let teamName = teamInput.value;
+        equipos[teamName] = [];
+
+        addTeamOption(teamName)
+
+        teamsList.innerHTML = "";
+
+        for (var key in equipos){
+
+            showTeams(key, equipos[key])
+            
+        }
+
+    }
+});
 
 
 addButton.addEventListener("click", (event) => { //Escucha el evento click y ejecuta una o varias acciones
     event.preventDefault(); //Previene el comportamiento usual de este evento
 
     if(nameInput.value != ""){
+
+        equipos[selectInput.value].push(nameInput.value)
+
+        console.log(equipos)
+
         let listItem = document.createElement("li");
 
         listItem.innerText = nameInput.value;
 
-        namesList.appendChild(listItem);
-
         nameInput.value = "";
+
+        teamsList.innerHTML = "";
+
+        for (var key in equipos){
+
+            showTeams(key, equipos[key])
+            
+        }
+
     }
     
 });
+
+
+function showTeams(name, members){
+
+
+    let teamContainer = document.createElement("div")
+        teamContainer.classList.add("equipo")
+        
+    let teamName = document.createElement("h3")
+        teamName.innerText = name
+
+    let teamList = document.createElement("ol")
+        teamList.classList.add("lista-equipo")
+
+    teamContainer.appendChild(teamName)
+    teamContainer.appendChild(teamList)
+
+    members.forEach( (member) => {
+        let teamMember = document.createElement("li")
+            teamMember.innerText = member
+
+            teamList.appendChild(teamMember)
+    })
+
+    teamsList.appendChild(teamContainer)
+
+    console.log(teamsList)
+}
